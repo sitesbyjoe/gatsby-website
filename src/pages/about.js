2c65ffb8253bcc32f45b8cf9e47d39c64b2967d6
Joseph R B Taylor <joe@sitesbyjoe.com>
2021-11-22 10:37:06 -0800

    opening commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Page</title>
+      <h1>About Page</h1>
+    </main>
+  )
+}
+
+export default AboutPage
